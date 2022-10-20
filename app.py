6e309d837b0fe89cf19139bc2e4eb6a4bc9d2fe1
Timur Rakhmatullin <rax_5@mail.ru>
2022-10-20 23:26:53 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import serial
+import threading
+import time
+import json
+import argparse
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)  # веб камера
+
+controlX, controlY = 0, 0  # глобальные переменные положения джойстика с web-страницы
+
+
+def getFramesGenerator():
+    """ Генератор фреймов для вывода в веб-страницу, тут же можно поиграть с openCV"""
+    while True:
+        time.sleep(0.01)    # ограничение fps (если видео тупит, можно убрать)
+        success, frame = camera.read()  # Получаем фрейм с камеры
+        if success:
+            frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_AREA)  # уменьшаем разрешение кадров (если видео тупит, можно уменьшить еще больше)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # перевод изображения в градации серого
+            # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)  # бинаризуем изображение
+            _, buffer = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """ Генерируем и отправляем изображения с камеры"""
+    return Response(getFramesGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """ html страница """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=5000, help="Running port")
+    parser.add_argument("-i", "--ip", type=str, default='172.20.10.3', help="Ip address")
+    args = parser.parse_args()
+
+    app.run(debug=False, host=args.ip, port=args.port)   # запускаем flask приложение
